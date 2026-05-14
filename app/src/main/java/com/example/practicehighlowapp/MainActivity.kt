dc86c3b7c3113d31ced85d4bb37b50b85252a6e1
itc-s24016 <s24016@std.it-college.ac.jp>
2026-05-14 14:51:16 +0900

    数字の表示と次のゲームへのボタンを実装

--- app/src/main/java/com/example/practicehighlowapp/MainActivity.kt
+++ app/src/main/java/com/example/practicehighlowapp/MainActivity.kt
@@ -1,5 +1,11 @@
 package com.example.practicehighlowapp
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.unit.sp
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -20,10 +26,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             PracticeHighLowAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Main(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +34,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Main(modifier: Modifier = Modifier) {
+    val com = remember { mutableIntStateOf((1..13).random()) }
+    val you = remember { mutableIntStateOf((1..13).random()) }
 
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+        Text(text = "相手のカード", fontSize = 20.sp)
+        Text(
+            text = com.value.toString(),
+            fontSize = 48.sp
+        )
+        Text(text = "あなたのカード", fontSize = 20.sp)
+        Text(
+            text = you.value.toString(),
+            fontSize = 48.sp
+        )
+        Button(onClick = {
+            com.intValue = (1..13).random()
+            you.intValue = (1..13).random()
+        }) {
+            Text(text = "次のゲーム", fontSize = 20.sp)
+        }
+    }
+}
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainPreview() {
     PracticeHighLowAppTheme {
-        Greeting("Android")
+        Main()
     }
 }
